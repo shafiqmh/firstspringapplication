8e2488ff0322ffa9b791a8d239600778e501e607
shafiqmh <mohammed.shafiq@academytrainee.com>
2019-09-10 14:53:40 +0100

    tested get all notes and get one note in notes controller

---
+++ src/test/java/com/qa/controllers/NotesControllerTest.java
@@ -0,0 +1,60 @@
+package com.qa.controllers;
+
+import com.qa.models.Note;
+import com.qa.repository.NotesRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class NotesControllerTest {
+
+
+    @InjectMocks
+    private NotesController notesController;
+
+    @Mock
+    private NotesRepository repository;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetAllNotes(){
+        List<Note> notesList = new ArrayList<>();
+        Note note = new Note();
+        note.setDescription("blah");
+        note.setName("blah");
+        notesList.add(note);
+        when(repository.findAll()).thenReturn(notesList);
+        assertEquals(notesController.listAllNotes().get(0).getName(),"blah");
+    }
+
+    @Test
+    public void testGetOneNote(){
+        //List<Note> notesList = new ArrayList<>();
+        Note note = new Note();
+        //note.setDescription("blurgh");
+        note.setName("blah");
+        //notesList.add(note);
+        when(repository.findOne(1L)).thenReturn(note);
+        notesController.getNote(1L);
+    }
+}
